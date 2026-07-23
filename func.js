695a50ccdc976465f469f6c7b50b2aa84ce4723c
Amogh Hosamani <amoghv.hosamani@gmail.com>
2026-07-23 13:13:43 +0530

    Delete func.js

--- func.js
+++
@@ -1,9 +0,0 @@
-function greetStudent(name, subject)
-{
-    let msg = "welcome" + name + "to" + subject
-    return msg;
-}
-
-//call function
-let result = greetTudent("ananya", "javascript")
-console.log(result);
